83330e9ef1ea477b0dc5103c35e42efb6d1b66a9
leerivera <leerivera80@gmail.com>
2021-03-22 11:29:39 -0400

    created a header and container

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 
+import { Container } from '@material-ui/core';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Header } from './components/Header/Header';
 
 function App() {
+
+   const [word, setWord] = useState("")
+   const [meanings, setMeanings] = useState([])
+
   const dictionaryApi = async () => {
     try{
       const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane"
       );
-      console.log(data);
+     
+      setMeanings(data.data);
 
     } catch (error){
       console.log(error)
     }
-  }
+  };
+
+  console.log(meanings);
 
   useEffect(() => {
     dictionaryApi();
 
   }, []);
   return (
-    <div className="App">
+    <div className="App" style ={{height: "100vh", backgroundColor:"#282c34", color:"white"}}>
+      <Container maxWidth="md" style={{display: "flex", flexDirection: "column", height: "100vh"}}>
+        <Header/>
+      </Container>
     
       
-      </div>
+    </div>
   );
 }
 
