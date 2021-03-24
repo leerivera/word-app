ca70e03df0c46ac2d4be78988774e16514b8eb64
leerivera <leerivera80@gmail.com>
2021-03-24 15:08:49 -0400

    created select and data for languages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import {  createMuiTheme, TextField, ThemeProvider } from '@material-ui/core'
+import {  createMuiTheme, MenuItem, TextField, ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import "./Header.css"
 
@@ -17,6 +17,31 @@ export const Header = () => {
             <div className="inputs">
               <ThemeProvider theme = { darkTheme }>
                <TextField id="standard-basic" label="Standard" />
+               <TextField
+                      id="standard-select-currency"
+                      select
+                      label="Select"
+                      
+                      helperText="Please select your currency"
+                    >
+                      
+                        <MenuItem >
+                          
+                          English
+                        </MenuItem>
+                      
+                    </TextField>
+                    <TextField
+                      id="standard-select-currency-native"
+                      select
+                      label="Native select"
+                      // value={}
+                      // onChange={}
+                      SelectProps={{
+                        native: true,
+                      }}
+                      helperText="Please select your currency"
+                    ></TextField>
               </ThemeProvider>
 
             </div>
